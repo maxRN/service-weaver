d87ac3b346b13048de03a3c7af613f615b01cac1
Max Große <max12.gro@gmail.com>
2023-05-28 19:11:21 +0200

    move services

--- scripts/start_trad.sh
+++ scripts/start_trad.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "starting main service..."
-go run ../trad-microservice/services/main-service/main.go 8070 8069 &
-go run ../trad-microservice/services/main-service/main.go 8071 8068 &
+go run ../trad-microservice/main-service/main.go 8070 8069 &
+go run ../trad-microservice/main-service/main.go 8071 8068 &
 
 echo "starting worker..."
-go run ../trad-microservice/services/work-service/main.go 8069 &
-go run ../trad-microservice/services/work-service/main.go 8068
+go run ../trad-microservice/work-service/main.go 8069 &
+go run ../trad-microservice/work-service/main.go 8068
