6b1a659c86569edb1e9cdc3dd32ef2d745b22be5
Max Große <max12.gro@gmail.com>
2023-06-13 16:45:19 +0200

    only do subset of tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,14 +4,16 @@ ip=$1
 # weaver or trad
 app=$2
 
-for n in 10000 50000 100000; do
-    for c in 1 10 20; do
+# for n in 10000 50000 100000; do
+#     for c in 1 10 20; do
         for ep in "long" "thick" "nothing"; do
             for test_run in 1 2 3; do
                 echo "running bench $app $n $c $ip $ep"
                 echo "test_run $test_run"
-                ./bench.sh $app $n $c $ip $ep $test_run
+                ./bench.sh $app 10000 1 $ip $ep $test_run
+                ./bench.sh $app 50000 10 $ip $ep $test_run
+                ./bench.sh $app 100000 20 $ip $ep $test_run
             done
         done
-    done
-done
+#     done
+# done
